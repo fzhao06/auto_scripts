b5e44c60a56b2d3055938ffc612c8d5b84c9219b
fzhao <felix.zhao@scaleflux.com>
2018-06-29 10:32:51 -0700

    handle yes/no ssh authentication

--- ssh_log.sh
+++ ssh_log.sh
@@ -1,4 +1,4 @@
-#!/bin/expect -f  
+#!/bin/expect -f
 set 1 [lindex $argv 0]
 set 2 [lindex $argv 1]
 set timeout 60
@@ -7,11 +7,19 @@ set timeout 60
 #expect "# " { send "mkdir -p .ssh\r" } 
 #expect "# " { send "exit\r" }
 spawn scp .ssh/id_rsa.pub $1@$2:~/id_rsa.pub
-expect "assword:" { send "scaleflux\r" }
-expect "# "
+expect {
+  "(yes/no" { 
+    send "yes\r" 
+    exp_continue
+  } 
+  "assword:" { 
+    send "scaleflux\r" 
+  } 
+}
+expect { "$ " "# " }
 spawn ssh $1@$2
 expect "assword:" { send "scaleflux\r" }
-expect "# " { send "mkdir -p .ssh\r" }
+expect "# " { send "mkdir -p ~/.ssh\r" }
 expect "# " { send "mv -f ~/id_rsa.pub .ssh\r" }
 expect "# " { send "cat .ssh/id_rsa.pub >> .ssh/authorized_keys\r" }
 expect "# "
